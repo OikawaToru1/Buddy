c15c85685125e05cef5cc7b9bd400b67848f4940
Aniket <aniketadhikari2@gmail.com>
2026-05-07 22:10:08 +0545

    created footer sample

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+  return (
+    <div className="flex justify-center items-center border-t  border-gray-900">
+        <p className="text-xs text-gray-400"> A cheap copy of NoteBook LM</p>
+    </div>
+  )
+}
+
+export default Footer
