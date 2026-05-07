b248ad7c96defcc5767c960e4bf311223fb3a115
Aniket <aniketadhikari2@gmail.com>
2026-05-07 22:02:43 +0545

    server file for backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,29 @@
 import express from 'express'
+import {upload} from './middlewares/multer.js'
+import cors from 'cors'
+import fileRouter from './routes/fileRouter.js'
 
 const app = express()
 const port = 3000
 
+app.use(cors());
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api/files', fileRouter);
+
+
+
+
+app.post('/api/files',upload.single('file'), (req, res) => {
+  console.log('Received file upload request');
+  console.log(req.file);
+  res.json({ message: 'File uploaded successfully', file: req.file });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
