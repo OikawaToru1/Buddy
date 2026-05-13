7c9704f8bd85d9ee48214178b3ced2b38ece8379
Aniket <aniketadhikari2@gmail.com>
2026-05-13 21:52:10 +0545

    added fix to responsivity

--- client/src/pages/Files.tsx
+++ client/src/pages/Files.tsx
@@ -4,11 +4,11 @@ import { useEffect } from "react"
 function Files() {
     useEffect(() => {
         axios.get("http://localhost:3000/api/files")
-        .then(res => console.log(res))
+        .then(res => console.log(res.data))
         .catch(err => console.log(err, "Error fetching files"))
     }, []);
   return (
-    <div className="bg-gray-800/50 mx-auto w-2/3 flex flex-col items-center justify-center  py-4 rounded-xl border border-gray-600/50 ">
+    <div className="bg-gray-800/50 mx-auto w-2/3 text-center flex flex-col items-center justify-center  py-4 rounded-xl border border-gray-600/50 ">
       List of Files Uploaded Previously !
     </div>
   );
