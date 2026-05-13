90ad43efac9fb25d90f9e542aab409e7584c07f5
Aniket <aniketadhikari2@gmail.com>
2026-05-13 21:51:29 +0545

    layout for buddy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Layout from './components/layout/Layout';
+import Hero from './pages/Hero';
+
 
 function App() {
 
 
   return (
-  <>
-  </>
-  )
+    <Layout>
+        <Hero />
+    </Layout>
+  );
 }
 
 export default App
