5c853e60d38f7c19013453295507ea3be34c1e57
Aniket <aniketadhikari2@gmail.com>
2026-05-17 13:13:21 +0545

    added router for handling query of a file

--- server/src/routes/fileRouter.ts
+++ server/src/routes/fileRouter.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
-import {getFileByName, uploadFile} from '../controllers/fileController.js'
+import {getFileByName, uploadFile, handleQuery} from '../controllers/fileController.js'
 import {upload} from '../middlewares/multer.js'
 
 const fileRouter = Router();
 
 fileRouter.get('/', (req, res) => {
   console.log('Received request for file list');
-  res.send('File list');
-})
+  res.json({ message: 'List of files uploaded previously' });
+});
 
 fileRouter.get('/file/:fileName', getFileByName);
 
 fileRouter.post("/",upload.single('file'),uploadFile);
+
+fileRouter.post("/query", handleQuery);
 
 
 
