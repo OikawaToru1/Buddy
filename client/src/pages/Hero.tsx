cfb23bf06f905e55a1c500eae68c2af027a449cb
Aniket <aniketadhikari2@gmail.com>
2026-05-17 12:57:27 +0545

    implemented test for tab navigation

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -1,8 +1,42 @@
 import UploadFile from "../components/ui/UploadFile"
 import Files from "./Files"
+import ChatBox from "../components/ui/ChatBox"
+import { useState } from "react";
+import TabNavigation from "../components/ui/TabNavigation";
 
+interface tabProps {
+  id : string,
+  label : string,
+  content: any,
+}
 
 function Hero() {
+
+  const tabs : tabProps[]  = [
+    {
+      id : 'uploadfile',
+      label : 'Upload File',
+      content : <UploadFile/>
+    },
+    {
+      id : 'files',
+      label : 'Files',
+      content : <Files/>
+    },
+    {
+      id: 'chatbox',
+      label : 'Chat box',
+      content : <ChatBox/>
+    }
+  ];
+
+  const TABS = {
+  'uploadfile' : <UploadFile/>,
+  'files' : <Files/>,
+  'chatbox' : <ChatBox/>
+  }
+  const[selectedTab, setSelectedTab] = useState<'uploadfile' | 'files' | 'chatbox'>('uploadfile');
+
   return (
     <div className="flex flex-col gap-4 items-center md:flex-row   w-full h-[90vh]  text-white">
         <div className="w-full md:w-1/3 h-full bg-gray-800/50 rounded-xl border border-gray-600/50 flex flex-col items-center gap-4">
@@ -10,10 +44,17 @@ function Hero() {
             <Files />
         </div>
 
-        <div className="w-full md:w-2/3 h-full bg-gray-800/50 rounded-xl border border-gray-600/50 flex items-center justify-center">
-            <h1>View and chat with Ai</h1>
-        </div>
+      <ChatBox/>
+        
     </div>
+
+    // Alternative UI with tab navigation
+  //   <div>
+  //     <TabNavigation setSelectedTab={setSelectedTab}/>
+  //     <div>
+  //       {TABS[selectedTab]}
+  //     </div>
+  //   </div>
   )
 }
 
