45f0bd81fb94adefd01c098e26a62b90cf2ade9d
Aniket <aniketadhikari2@gmail.com>
2026-05-18 13:52:40 +0545

    implemented express.json middleware for parsing json requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,23 @@
+import mongoose from 'mongoose'
 import express from 'express'
 import {upload} from './middlewares/multer.js'
 import cors from 'cors'
 import fileRouter from './routes/fileRouter.js'
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+// ;(async()=>{
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}`);
+//   } catch (error) {
+//     console.log("Error", error );
+//     throw error;
+//   }
+// })()
 
 app.use(cors());
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
@@ -18,11 +29,12 @@ app.use('/api/files', fileRouter);
 
 
 
-app.post('/api/files',upload.single('file'), (req, res) => {
-  console.log('Received file upload request');
-  console.log(req.file);
-  res.json({ message: 'File uploaded successfully', file: req.file });
-});
+// app.post('/api/files',upload.single('file'), (req, res) => {
+//   console.log('Received file upload request');
+//   console.log(req.file);
+  
+//   res.json({ message: 'File uploaded successfully', file: req.file });
+// });
 
 
 app.listen(port, () => {
