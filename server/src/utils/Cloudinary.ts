21c75f115ecb6b8283e773b0076036eeb3231af3
Aniket <aniketadhikari2@gmail.com>
2026-05-26 20:24:19 +0545

    wrote a function to upload and fetch file from cloudinary

---
+++ server/src/utils/Cloudinary.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+
+
+
+cloudinary.config({
+    secure: true,
+});
+
+
+export const uploadToCloudinary = async (localFilePath : string)=>{
+    try {
+        if(!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "raw",
+            use_filename: true,
+            unique_filename : false,
+            overwrite : true,
+        });
+
+        console.log("Succesfully uploaded file to cloudinary", response.secure_url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log("Error in uploading file to cloud",error);
+        return null;
+    }
+}
