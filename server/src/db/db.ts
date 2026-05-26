8d163b19d6c20da5da8290c00e41dcd9e5ad6ce0
Aniket <aniketadhikari2@gmail.com>
2026-05-26 20:21:26 +0545

    created a function to connect to mongoDB instance

---
+++ server/src/db/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from './constant.js';
+
+const conntectMongoDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !!!  :
+            \n ${connectionInstance.connection.host}
+            `);
+        
+    } catch (error) {
+        console.log("Error connecting to your MongoDB database", error);
+    }
+}
+export default conntectMongoDB
