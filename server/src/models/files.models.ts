9ba72a0adfa118a3c3231fca6e11b2c9da7cc42c
Aniket <aniketadhikari2@gmail.com>
2026-05-26 20:22:45 +0545

    created a structure for file in db models

---
+++ server/src/models/files.models.ts
@@ -0,0 +1,15 @@
+import {Schema, model} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const fileSchema = new Schema({
+    fileId : { type: String, required: true, unique: true, index: true },
+    filename : { type: String, required: true },
+    path : { type: String, required: true },
+    owner : { type: Schema.Types.ObjectId, ref: 'User', required: true },
+},{timestamps: true});
+
+fileSchema.plugin(mongooseAggregatePaginate);
+
+export const File = model("File",fileSchema);
+
+
