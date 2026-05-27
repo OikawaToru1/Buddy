40a5bb694486ef7db14fec2754040437efc0704f
Aniket <aniketadhikari2@gmail.com>
2026-05-27 16:54:22 +0545

    created option to add avatar for each user

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,25 +2,31 @@ import { useState } from "react";
 import { set } from "react-hook-form";
 
 interface SignUpProps {
+    fullName : string;
     username: string;
     email: string;
     password: string;
+    avatar?: File;
 }
 
 function Signup() {
     const [formData, setFormData] = useState<SignUpProps>({
+        fullName : '',
         username: '',
         email: '',
         password: '',
+        avatar: undefined,
     });
     const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(formData);
         if(formData.username && formData.email && formData.password) {
             setFormData({
+              fullName : '',
                 username: '',
                 email: '',
                 password: '',
+                avatar: undefined,
             });
             alert("Sign up successful!");
         } else {
@@ -35,6 +41,24 @@ function Signup() {
           className="w-full max-w-sm bg-gray-800/50 rounded-lg p-6"
           onSubmit={handleSignUp}
         >
+          <div className="mb-4">
+            <label
+              className="block text-gray-300 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Full Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="john doe"
+              value={formData.fullName}
+              onChange={(e) =>
+                setFormData({ ...formData, fullName: e.target.value })
+              }
+            />
+          </div>
           <div className="mb-4">
             <label
               className="block text-gray-300 text-sm font-bold mb-2"
@@ -89,6 +113,23 @@ function Signup() {
               }
             />
           </div>
+
+          <div className="mb-6 flex gap-4 items-center">
+            <label htmlFor="avatar">Avatar</label>
+            <input
+              id="avatar"
+              type="file"
+              accept="image/*"
+              className="text-gray-300  bg-gray-700 hover:bg-gray-600 focus:outline-none focus:shadow-outline rounded w-full py-2 px-3"
+              onChange={(e) => {
+                const file = e.target.files?.[0];
+                if (file) {
+                  setFormData({ ...formData, avatar: file });
+                }
+              }}
+            />
+          </div>
+
           <div className="flex flex-col gap-4 items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
