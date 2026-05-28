2dda4f6e8e77bd247e3543deec28f31ae5d7329c
Aniket <aniketadhikari2@gmail.com>
2026-05-28 16:06:09 +0545

    added routes for login and register

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,3 +1,16 @@
 import { Router } from "express";
+import {registerUser, loginUser} from "../controllers/authController.js";
+
+import {upload} from '../middlewares/multer.js'
 
 const authRouter = Router();
+
+authRouter.get('/', (req, res) => {
+  console.log('Received request for auth route');
+  res.json({ message: 'Auth route is working' });
+});
+
+authRouter.post("/register", upload.single("avatar"), registerUser);
+authRouter.post("/login", loginUser);
+
+export default authRouter;
