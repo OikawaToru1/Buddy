35d09022b88f3624e8ca1c6a1a846cfaa3499dec
Aniket <aniketadhikari2@gmail.com>
2026-06-04 14:51:42 +0545

    concept for state management of user

---
+++ client/src/rtk/slice/authSlice.ts
@@ -0,0 +1,35 @@
+import {createSlice , type PayloadAction} from "@reduxjs/toolkit";
+
+export interface AuthState {
+    isAuthenticated: boolean;
+    user: {
+        id: string;
+        username: string;
+        email: string;
+    } | null;
+}
+
+const initialState: AuthState = {
+    isAuthenticated: false,
+    user: null,
+};
+
+const authSlice = createSlice({
+    name : 'auth',
+    initialState,
+    reducers : {
+        loginSuccess(state, action : PayloadAction<AuthState['user']>) {
+            state.isAuthenticated = true;
+            state.user = action.payload;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+            state.user = null;
+        }
+    }
+});
+
+export const {loginSuccess, logout} = authSlice.actions;
+
+export default authSlice.reducer;
+
