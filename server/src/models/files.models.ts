e251f5493fe3de399c7b232be20d6511d0d170de
Aniket <aniketadhikari2@gmail.com>
2026-06-05 15:55:18 +0545

    added additional feild to model for file

--- server/src/models/files.models.ts
+++ server/src/models/files.models.ts
@@ -3,7 +3,7 @@ import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const fileSchema = new Schema({
     fileId : { type: String, required: true, unique: true, index: true },
-    filename : { type: String, required: true },
+    fileName : { type: String, required: true },
     path : { type: String, required: true },
     owner : { type: Schema.Types.ObjectId, ref: 'User', required: true },
 },{timestamps: true});
