cf28df0cc58c964952e2dce3453a161797e9952b
Aniket <aniketadhikari2@gmail.com>
2026-06-06 10:34:15 +0545

    changed the logic for receiving files and displaying it

--- client/src/pages/Files.tsx
+++ client/src/pages/Files.tsx
@@ -1,15 +1,23 @@
-import axios from "axios"
-import { useEffect } from "react"
 
-function Files() {
-    useEffect(() => {
-        axios.get("http://localhost:3000/api/files")
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err, "Error fetching files"))
-    }, []);
+function Files({files,setSelectedFile} : {files: {fileName: string, path: string}[], setSelectedFile: React.Dispatch<React.SetStateAction<{fileName: string, path: string} | null>>}) {
+
+
   return (
     <div className="bg-gray-800/50 mx-auto w-2/3 text-center flex flex-col items-center justify-center  py-4 rounded-xl border border-gray-600/50 ">
-      List of Files Uploaded Previously !
+     {files.length > 0 ? (
+        files.map((file, index) => (
+          <div key={index} className="bg-gray-700/50 w-full my-2 p-4 rounded-lg border border-gray-600/50"
+            onClick={() => setSelectedFile(file)}
+            >
+            <h2 className="text-xl font-semibold">{file.fileName}</h2>
+            <a href={file.path} target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">
+              View File
+            </a>
+          </div>
+        ))
+      ) : (
+        <p>No files uploaded yet.</p>
+      )}
     </div>
   );
 }
