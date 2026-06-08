ec501061d5d706d8f670121f69fbd8a418998a82
Aniket <aniketadhikari2@gmail.com>
2026-06-08 12:55:23 +0545

    removed test commits

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,9 +4,9 @@ import { DB_NAME } from './constant.js';
 const conntectMongoDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected !!!  :
-            \n ${connectionInstance.connection.host}
-            `);
+        // console.log(`\n MongoDB connected !!!  :
+        //     \n ${connectionInstance.connection.host}
+        //     `);
         
     } catch (error) {
         console.log("Error connecting to your MongoDB database", error);
