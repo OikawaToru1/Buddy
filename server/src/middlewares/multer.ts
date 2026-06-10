9dd9a749079cf27dc978fbcae53390453d07d4cf
Aniket <aniketadhikari2@gmail.com>
2026-06-10 17:22:48 +0545

    changed config of middleware from disk storage to memory

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,13 +1,19 @@
 import multer from 'multer'
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
-  },
-  filename: function (req, file, cb) {
+// Code below is for local storage(disk storage), it means the file will be stored in local file system of our server
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
     
-    cb(null, file.originalname);
-  },
-});
+//     cb(null, file.originalname);
+//   },
+// });
+
+//Below is code for memory storage, it means the file will be stored in memory as a buffer and we can directly upload it to cloudinary without storing it in local file system of our server
+
+const storage = multer.memoryStorage();
 
 export const upload = multer({ storage: storage, });
